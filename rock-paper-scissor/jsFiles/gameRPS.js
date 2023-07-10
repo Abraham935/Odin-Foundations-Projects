7301896f41459334a3381267732d82176776499e
Abraham935 <Abraham_935@hotmail.com>
2023-07-10 11:11:17 -0600

    Finished functions for playing the game

---
+++ rock-paper-scissor/jsFiles/gameRPS.js
@@ -0,0 +1,36 @@
+function getComputerChoice (){
+
+    let choices = ["rock", "paper", "scissors"];
+    
+    let randomIndex = Math.floor(Math.random() * choices.length);
+
+    //console.log(choices[randomIndex]);
+    return choices[randomIndex];
+
+}
+
+function playerTurn(playerSelection, computerSelection){
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    //Tie
+    if(playerSelection == computerSelection){
+        console.log(`Tie! Both players selection were ${playerSelection} and  ${computerSelection}`)
+    }
+    else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")){
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`)
+
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+
+    }
+}
+
+for(let i = 0; i <= 5; i++ ){
+    let choiceComputer = getComputerChoice();
+
+    let choice = prompt("Enter your choice");
+
+    playerTurn(choice, choiceComputer);
+
+}
