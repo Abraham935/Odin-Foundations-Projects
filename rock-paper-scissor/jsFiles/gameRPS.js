0e790a5af831a611ab086db1bae9bf761f118007
Abraham935 <Abraham_935@hotmail.com>
2023-07-12 19:07:50 -0600

    Added that buttons now add points to player and computer when clicking them

--- rock-paper-scissor/jsFiles/gameRPS.js
+++ rock-paper-scissor/jsFiles/gameRPS.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let buttons = document.querySelectorAll('button');
+
 function getComputerChoice (){
 
     let choices = ["rock", "paper", "scissors"];
@@ -9,21 +13,37 @@ function getComputerChoice (){
 
 }
 
+function displayWinner(playerScore, computerScore){
+    if(playerScore == 5){
+        console.log("Player Wins");
+    }
+    else if(computerScore == 5){
+        console.log("Computer wom");
+    }
+}
+
 function playerTurn(playerSelection, computerSelection){
     console.log(playerSelection);
     console.log(computerSelection);
 
-    //Tie
     if(playerSelection == computerSelection){
         console.log(`Tie! Both players selection were ${playerSelection} and  ${computerSelection}`)
     }
     else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")){
         console.log(`You won! ${playerSelection} beats ${computerSelection}`)
+        playerScore++;
+    
 
     } else {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        computerScore++
 
     }
+    
+    console.log(playerScore);
+    console.log(computerScore)
+
+    displayWinner(playerScore, computerScore);
 }
 
 /*for(let i = 0; i <= 5; i++ ){
@@ -34,3 +54,16 @@ function playerTurn(playerSelection, computerSelection){
     playerTurn(choice, choiceComputer);
 
 }*/
+
+
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        let computerChoice = getComputerChoice()
+        let playerChoice = button.value;
+        playerTurn(playerChoice, computerChoice)
+    });
+  })
